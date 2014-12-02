4c1e90fd6dc33c3031b5e569d6240d772a6ae8a1
Benjamin Staffin <ben@folsomlabs.com>
2014-12-01 18:12:42 -0800

    Expanded vagrantfile, now with provisioning.
    Change-Id: I647e19e44d46f3e6bf9ab8125c0bf0fce49ae179

--- Vagrantfile
+++ Vagrantfile
@@ -5,13 +5,26 @@ VAGRANTFILE_API_VERSION = '2'
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = 'ubuntu/trusty64'
-  config.cache.auto_detect = true
+
+  if Vagrant.has_plugin?('vagrant-cachier')
+    config.cache.auto_detect = true
+    config.cache.scope = :box
+    config.cache.synced_folder_opts = {
+      type: :nfs,
+      mount_options: [:rw, :tcp]
+    }
+  end
+
+  unless Vagrant.has_plugin?('vagrant-hostsupdater')
+    fail 'Please install the vagrant-hostsupdater plugin.'
+  end
+
   config.vm.define 'debtest' do |deb|
     deb.vm.network :private_network, ip: '192.168.33.8'
     deb.vm.hostname = 'aurora-debtest'
     deb.vm.provider :virtualbox do |vb|
       vb.customize ['modifyvm', :id, '--memory', '1024']
     end
-    deb.vm.synced_folder '../build-area', '/debs', type: 'nfs'
+    deb.vm.provision 'shell', path: 'vagrant-provision.sh'
   end
 end
---
+++ vagrant-provision.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 21E1FA28 E56151BF
+cat > /etc/apt/sources.list.d/vagrant.list <<EOF
+deb http://folsom-labs-artifacts.s3.amazonaws.com/apt trusty main
+deb http://repos.mesosphere.io/ubuntu trusty main
+EOF
+apt-get update
+apt-get -qfy install pbuilder git-buildpackage
+
+cd /vagrant
+/usr/lib/pbuilder/pbuilder-satisfydepends
+mkdir -p "$HOME/build-area"
+git-buildpackage --git-export-dir="$HOME/build-area" -us -uc --git-ignore-new
+
+cd "$HOME/build-area"
+dpkg-scanpackages . > Packages
+
+cat > /etc/apt/sources.list.d/local.list <<EOF
+deb [trusted=yes] file://$HOME/build-area ./
+EOF
+
+apt-get update
+apt-get -qfy install aurora-scheduler aurora-mesos-slave aurora-tools
+
+service aurora-scheduler stop
+service aurora-mesos-slave stop
+service mesos-master stop
+service mesos-slave stop
+
+mkdir -p /etc/aurora
+cat > /etc/aurora/clusters.json <<EOF
+[{
+"name": "example",
+"zk": "192.168.33.8",
+"scheduler_zk_path": "/aurora/scheduler",
+"auth_mechanism": "UNAUTHENTICATED",
+"slave_run_directory": "latest",
+"slave_root": "/var/lib/mesos"
+}]
+EOF
+
+mkdir -p /var/lib/aurora/scheduler/db
+mesos-log initialize --path=/var/lib/aurora/scheduler/db
+
+mkdir -p /etc/mesos-slave/attributes
+echo "/var/lib/mesos" > /etc/mesos-slave/work_dir
+echo "1" > /etc/mesos-slave/attributes/rack
+echo "$HOSTNAME" > /etc/mesos-slave/attributes/host
+
+service mesos-master start
+service mesos-slave start
+service aurora-scheduler start
+service aurora-mesos-slave start
