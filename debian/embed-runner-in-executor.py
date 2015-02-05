f79c18f23d105e7468dba5efaa250d115564f037
Benjamin Staffin <ben@folsomlabs.com>
2015-02-04 21:14:51 -0800

    More updates for 0.7.0 features
    * Expose more parameters in /etc/default/aurora-scheduler for container
      types and Thermos executor flags/resources.
    * Eliminate the thermos_executor.sh wrapper script in favor of using
      -thermos_executor_flags on the scheduler.
    * Don't package /var/run/thermos; create it at runtime.
    * Mildly cleanup embed-runner-in-executor.py
    
    Change-Id: Ia640ff11912ca9b76682250f081c5804a1aeef1d

--- debian/embed-runner-in-executor.py
+++ debian/embed-runner-in-executor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Package runner within executor."""
+"""Package thermos_runner within thermos_executor."""
 
 import contextlib
 import zipfile
 
-with contextlib.closing(
-    zipfile.ZipFile('dist/thermos_executor.pex', 'a')) as zf:
- zf.writestr('apache/aurora/executor/resources/__init__.py', '')
- zf.write('dist/thermos_runner.pex',
-          'apache/aurora/executor/resources/thermos_runner.pex')
+with contextlib.closing(zipfile.ZipFile('dist/thermos_executor.pex',
+                                        'a')) as zf:
+    zf.writestr('apache/aurora/executor/resources/__init__.py', '')
+    zf.write('dist/thermos_runner.pex',
+             'apache/aurora/executor/resources/thermos_runner.pex')
